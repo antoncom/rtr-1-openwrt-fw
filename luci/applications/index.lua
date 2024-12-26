9902e4bf951df05f4c93f593fc8d2a41932c1341
Anton Gavrilyuk <amigoalt@gmail.com>
2024-12-26 10:06:54 +0300

    PPTP Client luci app fixed.

--- luci/applications/index.lua
+++ luci/applications/index.lua
@@ -1,47 +1,45 @@
 -- Copyright 2015 Daniel Dickinson <openwrt@daniel.thecshore.com>
 -- Licensed to the public under the Apache License 2.0.
 
-module("luci.controller.pptp-client.index", package.seeall)
+module("luci.controller.vpnconfig.index", package.seeall)
 
 local http = require "luci.http"
 local uci = require "luci.model.uci".cursor()
-local util = require "luci.util"
-local sys  = require "luci.sys"
-local ubus = require "ubus"
-local conn = ubus.connect()
 
-
-local dummy_cfg_content = 'config pptp \'pptp1\'\n'
+local dummy_cfg_content = 'config pptp \'pptp1\'\n\nconfig l2tp \'l2tp1\'\n'
 local uploadDir = "/etc/config/vpnconfig_files"
 
 function index()
-	if not nixio.fs.access("/etc/config/pptp-client") then
-		nixio.syslog("crit", "create file /etc/config/pptp-client")
-		nixio.fs.writefile("/etc/config/pptp-client", dummy_cfg_content);
+	if not nixio.fs.access("/etc/config/vpnconfig") then
+		nixio.syslog("crit", "create file /etc/config/vpnconfig")
+		nixio.fs.writefile("/etc/config/vpnconfig", dummy_cfg_content);
 	end
 	if not nixio.fs.access("/etc/config/vpnconfig_files") then
 		nixio.syslog("crit", "create directory ".. uploadDir)
 		nixio.fs.mkdir(uploadDir);
 	end
 
-	entry({"admin", "services", "pptp-client"},   cbi("pptp-client/pptp"),   _("PPTP Client"), 30).leaf = true
-	entry({"admin", "services", "pptp-client_2", "action"}, call("do_action"), nil).leaf = true
-
+	entry({"admin", "services", "vpnconfig", "action"}, call("do_action"), nil).leaf = true
 	
+	--entry({"admin", "services", "pptp"},   cbi("vpnconfig/pptp"),   _("PPTP Client"), 30).leaf = true
+	entry({"admin", "services", "l2tp"},   cbi("vpnconfig/l2tp"),   _("L2TP Client"),   40).leaf = true
+	entry({"admin", "services", "gre"},    cbi("vpnconfig/gre"),    _("GRE Client"),    50).leaf = true
+	entry({"admin", "services", "dmvpn"},  cbi("vpnconfig/dmvpn"),  _("DMVPN Tunnels"),  60).leaf = true
+	entry({"admin", "services", "eoip"},   cbi("vpnconfig/eoip"),   _("EoIP Tunnels"),   70).leaf = true
+	entry({"admin", "services", "l2tpv3"}, cbi("vpnconfig/l2tpv3"), _("L2TPv3 Tunnels"), 80).leaf = true
+	entry({"admin", "services", "ipsec"},  cbi("vpnconfig/ipsec"),  _("IPSec Tunnels"),  90).leaf = true
+
 end
 
 function do_action(action)
-	local config = 'network'
+	local config = 'vpnconfig'
 	local data = {}
-	local payload = luci.jsonc.parse(luci.http.content())
-
-	data.vpnType = payload["type"]
-	data.name = payload["name"]
-	data.isActive = payload["isActive"]
-	if payload["options"] then
-		data.options = payload["options"]
+	data.vpnType = luci.http.formvalue("type")
+	data.name = luci.http.formvalue("name")
+	data.isActive = luci.http.formvalue("isActive")
+	if luci.http.formvalue("options") then
+		data.options = luci.jsonc.parse(luci.http.formvalue("options"))
 	end
-
 
 	local commands = {
 		add= function(...)
@@ -53,7 +51,7 @@ function do_action(action)
 
 		edit = function(...)
 			for key, value in pairs(data.options) do
-				uci:set(config, 'vpnpptp', key, value)
+				uci:set(config, data.name, key, value)
 			end
 			return run_backend_script(action, data)
 		end,
@@ -63,11 +61,8 @@ function do_action(action)
 			return run_backend_script(action, data)
 		end,
 
-		activate = function(...)
+		enable = function(...)
 			uci:set(config, data.name, 'isActive', data.isActive)
-			for key, value in pairs(data.options) do
-				uci:set(config, 'vpnpptp', key, value)
-			end
 			return run_backend_script(action, data)
 		end,
 
@@ -114,10 +109,8 @@ function do_action(action)
 		end
 	
 		http.status(code)
-		--http.prepare_content("text/json")
-		--http.write_json(response)
-		http.prepare_content("application/json")
-		http.write(util.serialize_json(response))
+		http.prepare_content("text/json")
+		http.write_json(response)
 	end
 
 	if commands[action] then
@@ -125,13 +118,6 @@ function do_action(action)
 		if (success) then
 			send_response(200, "Configuration applyed successfully")
 			uci:commit(config)
-			if (data.isActive == '0') then
-				conn:call("network.interface.vpnpptp", "down", {})
-			elseif (data.isActive == '1') then
-				conn:call("network.interface.vpnpptp", "up", {})
-			end
-			conn:close()
-
 		else
 			send_response(500, "Failed to apply configuration")
 			uci:revert(config)
